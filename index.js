cd8cfcc56b9d644c4266264e71300a07e00720b3
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2023-09-09 12:04:51 +0200

    Add tooltip with close button

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 const boxes = document.querySelectorAll('.boxes div');
 
+let tooltipWithBtn = false; // tooltip with btn, exist check
+
 const createTooltip = (e) => {
-    const tooltipParent = e.target;
-    const tooltipText = e.target.dataset.tooltip;
-    const tooltipPosition = e.target.dataset.tooltipPosition;
+    const tooltipState = e.target.dataset.tooltipState;
+    if (!tooltipWithBtn) {
+        const tooltipParent = e.target;
+        const tooltipText = e.target.dataset.tooltip;
+        const tooltipPosition = e.target.dataset.tooltipPosition;
 
-    const newTooltip = document.createElement('span');
-    newTooltip.innerHTML = tooltipText;
-    newTooltip.className = `tooltip ${tooltipPosition || 'top'}`;
+        const newTooltip = document.createElement('span');
+        newTooltip.innerHTML = tooltipText;
+        newTooltip.className = `tooltip ${tooltipPosition || 'top'}`;
 
-    tooltipParent.appendChild(newTooltip);
+        tooltipParent.appendChild(newTooltip);
+
+        if (tooltipState == 'static') {
+            const closeBtn = document.createElement('button');
+            closeBtn.innerHTML = 'x';
+            closeBtn.className = 'close-btn';
+
+            closeBtn.addEventListener('click', removeTooltipByBtn);
+
+            newTooltip.appendChild(closeBtn);
+
+            tooltipWithBtn = true;
+        }
+    }
 };
 
 const removeTooltip = (e) => {
     const tooltip = e.target.querySelector('.tooltip');
-    if (tooltip) { tooltip.remove(); }
-    console.log(tooltip);
+    const tooltipState = e.target.dataset.tooltipState;
+    if (!tooltipState && tooltip) { tooltip.remove(); }
 };
+
+const removeTooltipByBtn = (e) => {
+    const tooltip = e.target.parentNode;
+    tooltip.remove();
+
+    tooltipWithBtn = false;
+}
 
 boxes.forEach((box) => box.addEventListener('mouseover', createTooltip));
 boxes.forEach((box) => box.addEventListener('mouseleave', removeTooltip));
